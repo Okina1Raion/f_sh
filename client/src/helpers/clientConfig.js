78004df6e9e4175f26fb1b1fe51e64789d79859d
Zinich Aleksandr <sany007x3@gmail.com>
2018-07-09 16:49:38 +0300

    init

---
+++ client/src/helpers/clientConfig.js
@@ -0,0 +1,12 @@
+function loadConfig() {
+    let configData = require('../../config/config.json');
+
+    configData.isDevLocal = process.env.NODE_ENV === 'development';
+
+    return configData;
+}
+
+let configValues = loadConfig();
+
+export default configValues;
+
